73a1349abf0d2533b79c73417c3d1b599ea68b65
veritablyClean <dcole617@gmail.com>
2015-05-05 22:21:25 -0400

    added wikia formatting

--- dateSort.pl
+++ dateSort.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 
-#command line $perl dateSort.pl fileToBeSorted fileToBeWritten
+#command line $perl dateSort.pl fileToBeSorted fileToBeWritten formattedFile
 $fileIn = $ARGV[0];
 $fileOut = $ARGV[1];
+$fFileOut = $ARGV[2];
 
 my %dateHash;
 
@@ -27,7 +28,7 @@ while (my $lineIn = <$fhIn>){
 		print "$shwartzDate";
 
 		#add bit to date
-		push( @{ $dateHash {$schwartzDate} }, "$title [$length]\n$uploader [$views views / $likes likes]\n$link\n");
+		push( @{ $dateHash {$schwartzDate} }, "$title\n$uploader [$views views / $likes likes]\n$link [$length]\n");
 	}
 }
 close $fhIn;
@@ -40,13 +41,58 @@ foreach my $key (sort {$a<=>$b} keys %dateHash){
 	$fKey = $key;
 	substr ($fKey, 4, 0,"/");
 	substr ($fKey, 7, 0,"/");
-	print $fhOut "***\n$fKey\n\n";
+	print $fhOut "***\n$fKey\n";
 
   foreach my $element (@{ $dateHash{$key}}){
 
 	#print bits
-	print $fhOut "$element\n\n";
+	print $fhOut "$element\n";
   }
 	
 }
 close $fhOut;
+
+# prints wiki formatting
+$yearCount = 1999;
+$monthCount = 11;
+
+my @months = qw(month January February March April May June July August September October November December);
+
+open (my $fhIn, '<', $fileOut) or die "file in error\n";
+open (my $fhOut, '>', $fFileOut) or die "final file out error\n";
+
+# float table of contents to rights
+print $fhOut "{{tocright}}\n";
+
+while (my $row = <$fhIn>){
+	chomp $row;
+	if ($row eq '***'){
+		$nextRowDate = 1;
+		next;
+	}
+	if ($nextRowDate == 1){
+		
+		# next row won't be a date
+		$nextRowDate = 0;
+
+		($yyyy, $mm, $dd) = split ("/", $row);
+
+		# push year header
+		if ($yyyy > $yearCount){
+			print $fhOut "=='''$yyyy'''==\n";
+			$yearCount++;
+			$monthCount = 0;
+		}
+
+		# creates month subheaders
+		if ($mm > $monthCount){
+			print $fhOut "==='''$months[$mm]'''===\n";
+			$monthCount = $mm;
+		}
+		
+		print $fhOut "'''$row'''\n";
+		next;
+	}
+
+	print $fhOut "$row<br>\n";
+}
